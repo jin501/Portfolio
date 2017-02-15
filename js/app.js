cb31ab9cf940b2a660b345a7b5cc255d17c06b97
jin501 <soljin01@gmail.com>
2017-02-15 01:41:17 -0500

    App.html created. Laid out basic FullPage.js capability

---
+++ js/app.js
@@ -0,0 +1,25 @@
+(function($) {
+  "use strict";
+  $(window).on("load", function() { // makes sure the whole site is loaded
+    //preloader
+    $("#status").delay(2000).fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
+
+  });
+
+
+  $(document).ready(function(){
+     $('#fullpage').fullpage();
+
+    //typed js
+    $(".typed").typed({
+        strings: ["My Name is Sol", "I'm a Developer"],
+        typeSpeed: 100,
+        backDelay: 900,
+        // loop
+        loop: true
+    });
+  });
+
+
+})(jQuery);
