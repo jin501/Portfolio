8b040857a1593197abf538a1550c8f7e52373bae
jin501 <soljin01@gmail.com>
2017-02-20 21:12:14 -0500

    resume slides

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+function showResume(){
+  $('#myModal').modal('show: true')
+
+}
 (function($) {
   "use strict";
   $(window).on("load", function() { // makes sure the whole site is loaded
@@ -6,12 +10,15 @@
     $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
 
   });
+  function showResume(){
+    debugger
+  }
 
 
   $(document).ready(function(){
      $('#fullpage').fullpage({
        anchors: ['home', 'about', 'resume', 'portfolio'],
-       menu: 'myMenu'
+       menu: 'myMenu',
      });
 
     //typed js
@@ -23,6 +30,5 @@
     });
 
   });
-
 
 })(jQuery);
