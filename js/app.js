d8665b4d7a94df8bcd4ce6cde606f2963691163f
jin501 <soljin01@gmail.com>
2017-02-22 03:41:40 -0500

    fixed some minor styling issues on resume slide #1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-function showResume(){
-  $('#myModal').modal('show: true')
-
-}
 (function($) {
   "use strict";
+  console.log("%c    \u2600SOL          ", "background: pink; color: yellow; font-size: x-large");
+  console.log("%c  solthewebdev@gmail.com   ", "background: pink; color: yellow; font-size: medium");
+  console.log("%c          \u00a9 2017        ", "background: pink; color: yellow; font-size: large");
+
   $(window).on("load", function() { // makes sure the whole site is loaded
     //preloader
     $("#status").delay(2000).fadeOut(); // will first fade out the loading animation
@@ -23,7 +23,7 @@ function showResume(){
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is Sol.", "I'm a Web Developer."],
+        strings: ["My name is Sol ", "I'm a &lt; <span style='color:green'>Web Developer</span> / &gt; "],
         typeSpeed: 100,
         backDelay: 900,
         loop: true
