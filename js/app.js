ff7f10a3ff7f1d5adfe21e6543b006c50fcf6b8d
jin501 <soljin01@gmail.com>
2017-02-26 04:04:35 -0500

    Chang text in resume section

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@
 
   $(window).on("load", function() { // makes sure the whole site is loaded
     //preloader
-    debugger
-    $("#status").delay(2000).fadeOut(); // will first fade out the loading animation
+
+    $("#status").delay(1000).fadeOut(); // will first fade out the loading animation
     $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
 
   });
@@ -16,11 +16,29 @@
   }
 
 
+
   $(document).ready(function(){
+    if ( $(window).width() < 800 || window.Touch) {
+        var $sensitivity = 10;
+    } else{
+        var $sensitivity = 100;
+    }
      $('#fullpage').fullpage({
        anchors: ['home', 'resume', 'portfolio'],
-       menu: 'myMenu'
+       menu: 'myMenu',
+      //  verticalCentered: true,
+       scrollOverflow:true,
+       normalScrollElements: '#my-resume, #ed-ex, #tech', '#coding',
+      //  paddingBottom: '0',
+      //  scrollingSpeed: '800',
+      //  navigation: true,
+      //  navigationPosition: 'right',
+      //  fixedElements:'#header',
+      //  resize: false,
+      //  css3: true,
+       touchSensitivity: $sensitivity
      });
+
 
     //typed js
     $(".typed").typed({
